0a74d3bea49560c12c060cd5cb0daab762ab80c1
Hack Reactor Students <accounts@hackreactor.com>
2015-09-25 19:31:32 -0700

    Crime events render in correct locations on map

--- client/maps/mapController.js
+++ client/maps/mapController.js
@@ -1,18 +1,18 @@
 angular.module('crimevis.maps', [])
 
 .controller('mapController', function($scope, Events) {
-  $scope.data = {};
-  $scope.data.events = [];
+  $scope.data;
   // limit the number of requests to the server
-  var requests = 0;
+  // var requests = 0;
   $scope.getEvents = function() {
     Events.fetchEvents().then(function (resp) {
-      $scope.data.events.push(resp.data);
-      requests++;
+      console.log(resp.data);
+      $scope.data = resp.data;
+      // requests++;
       // add data to the events array while requests are less than 10
-      if (requests <= 10) {
-        $scope.getEvents();
-      }
+      // if (requests <= 10) {
+      //   $scope.getEvents();
+      // }
     });
   };
   $scope.getEvents();
--- server/app.js
+++ server/app.js
@@ -10,11 +10,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var events = require('./routes/events');
 var app = express();
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing  your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
