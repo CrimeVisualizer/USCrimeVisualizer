eea728c91bee31d3cecf55c37b4dd9a6b49d92ca
tksander <tkepasander@gmail.com>
2015-09-30 13:57:53 -0700

    Added underscore to bower. Adding in heatmap functions to app.js. Added CSS styles for heatmap to style.css

--- jqueryclient/app.js
+++ jqueryclient/app.js
@@ -1,11 +1,103 @@
 var svg, projection;
 
+
+
 var getData = function (callback, params) {
   console.log('got in ajax request');
   $.get('/api/events/' + params , function (data) {
     callback(data);
   });
 };
+
+// Get Google Reverse Geocode API data - passes latLong for Google API lookup
+// https://developers.google.com/maps/documentation/geocoding/intro?csw=1#ReverseGeocoding
+var getZipcode = function (latLong, callback) {
+  console.log('Got the Google Zipcode data');
+
+  // Build URL string for query
+  var googleURL = '//maps.googleapis.com/maps/api/geocode/json?latlng=' 
+                  + latLong
+                  + '&key=AIzaSyDNluL4ihqXG-0jDzo-kGNfEy73pWRH9uI';
+
+  $.get(googleURL , function (data) {
+    console.log(data);
+    console.log(data.results[7]["long_name"]);
+    callback(data);
+  });
+
+
+// Calculates min and max of crime data set for use in quantize
+var dataMax = _.max(data, function(item) {
+  return item['count'];
+});
+var dataMin = _.min(data, function(item) {
+  return item['count'];
+});
+
+
+// Generates a range of 9 values within the provided domain.
+// Function used to generated CSS class values, 0-8.
+// Values correspond to a range of blues in CSS file (see CSS file)
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHANGE THE DOMAIN HARDCODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var quantize = d3.scale.quantize()
+    .domain([162, 622])
+    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
+// Append zipcode to each record
+// Uses Google API
+var appendZipcode = function(data) {
+
+  // Iterate over each record in the data
+  _.each(data, function(record) {
+
+    // Get lat & long from data
+    var latLong = record['Location'];
+    console.log(latLong);
+
+    // Call Google API with record's lat and long
+    getZipcode(latLong, function (zipcode) {
+
+      console.log(zipcode);
+      data['zipcode'] = zipcode;
+      console.log(data);
+    });
+  });
+};
+
+// Sum each zipcode count by each record
+var getZipcodeCount = function (data) {
+  var zipcodeCount = _.countBy(data, function(record) {
+    return record['zipcode'];
+  });
+};
+
+var renderHeatMap = function (params) {
+  getData(function(data) {
+    // Get zipcode for each entry and append to dataset
+    getZipcode()
+    appendZipcode()
+    // Run summing function on zipcode data
+    getZipcodeCount()
+
+    // Render the heat map
+    svg.selectAll("path")
+      .data(data.features)
+    .enter().append("path")
+      .attr("d", path)
+        .attr('data-id', function(d) {
+          return d.id;
+        }).attr('data-name', function(d) {
+          return d.properties.name;
+        })
+        // .style("fill", "#ffffff").style("stroke", "#111111")   REMOVE ME AFTER TESTING!
+        .attr("class", function(d) {
+          return quantize(districtData[d.properties.name]); // Quantize by total crimes per zipcode
+        });
+
+  });
+};
+
+
 
 var renderPoints = function (params) {
   getData(function (data) {
