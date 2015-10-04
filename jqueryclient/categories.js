667f752d85fed7f59d09a71772851d61efc52864
tscheys <tim.scheys@gmail.com>
2015-10-04 14:48:05 -0700

    Add documentation to front end

--- jqueryclient/categories.js
+++ jqueryclient/categories.js
@@ -1,9 +1,9 @@
 var categories = ["ROBBERY", "LARCENY/THEFT", "ASSAULT"];
-
+// make buttons for every crime category
 categories.forEach(function(value) {
   $("#categories").append("<button class=category>" + value + "</button>");
 });
-
+// on hover display only those crimes within that category
 $(".category").mouseenter(function () {
   var category = $(this).text();
   svg.selectAll("circle")
@@ -13,8 +13,7 @@ $(".category").mouseenter(function () {
     }
   })
 });
-
-
+// restore back to a map with all crimes on louse leave
 $(".category").mouseleave(function () {
   svg.selectAll("circle")
   .attr("r", "1px");
--- jqueryclient/timeline.js
+++ jqueryclient/timeline.js
@@ -1,10 +1,10 @@
+// make buttons for months and years
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var makeYears = function() {
   for(var i = 2003; i < 2016; i++) {
     $("#timeline").append("<button class='year' id=" + i + ">"+ i +"</button>");
   }
 }
-
 var makeMonths = function() {
   months.forEach(function(value, index) {
     var monthNumber = index + 1;
@@ -13,7 +13,7 @@ var makeMonths = function() {
 }
 makeYears();
 makeMonths();
-
+// IMPORTANT: below function will fire renderpoints() (in app.js) with certain params
 var getMonth = function() {
   console.log('getting data');
   var year = $('.active.year').attr("id");
@@ -21,12 +21,13 @@ var getMonth = function() {
   console.log(year, month);
   renderPoints('date=' + year + '-' + month);
 }
-
+// click events for year and month
 $(".year").on("click", function(e) {
   $(".year").removeAttr("class", "active");
   $(this).attr("class", "active year");
   console.log(this);
 });
+// clicking the month will fire getMonth() function
 $(".month").on("click", function(e) {
   $(".month").removeAttr("class", "active");
   $(this).attr("class", "active month");
