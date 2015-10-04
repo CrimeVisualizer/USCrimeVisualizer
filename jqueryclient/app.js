2343a1bca6eb94d87aa8bd2c831140fbe269be13
tksander <tkepasander@gmail.com>
2015-10-04 11:58:44 -0700

    Moved heatmap function invocationt to heatmap.js

--- jqueryclient/app.js
+++ jqueryclient/app.js
@@ -8,30 +8,6 @@ var getData = function (callback) {
 };
 
 
-// Calculates min and max of crime data set for use in quantize
-var getMaxMin = function (data) {
-  var resultArray = [];
-  var dataMax = _.max(data, function(item) {
-    return item;
-  });
-  var dataMin = _.min(data, function(item) {
-    return item;
-  });
-  resultArray.push(dataMin);
-  resultArray.push(dataMax);
-  // console.log(resultArray);
-  return resultArray;
-};
-
-
-
-// Sum each zipcode count by each record
-var getZipcodeCount = function (data) {
-  return _.countBy(data, function(record) {
-    return record['zipcode'];
-  });
-};
-
 var renderPoints = function (params) {
   getData(function (data) {
       var coord;
@@ -82,82 +58,6 @@ var animatePoints = function() {
   .attr("r", "0px");
 };
 
-// Renders Heat Map - this is rendered in place of standard map
-var renderHeatMap = function (params) {
-
-  // // Create the D3 map
-  // var width = .8 * window.innerWidth, height = .85 * window.innerHeight;
-
-  // // Creates the map svg
-  // svg = d3.select('#city').append("svg").attr("width", width).attr("height", height);
-
-  // // Map of San Francisco
-  // projection = d3.geo.mercator().scale(1).translate([0, 0]).precision(0);
-  // var path = d3.geo.path().projection(projection);
-
-  // // gsfmap is a global variable from map/map.js
-  // var bounds = path.bounds(gsfmap);
-
-  // xScale = width / Math.abs(bounds[1][0] - bounds[0][0]);
-  // yScale = height / Math.abs(bounds[1][1] - bounds[0][1]);
-  // scale = xScale < yScale ? xScale : yScale;
-
-  // var transl = [(width - scale * (bounds[1][0] + bounds[0][0])) / 2, (height - scale * (bounds[1][1] + bounds[0][1])) / 2];
-  // projection.scale(scale).translate(transl);
-
-  // Import data
-  var aggregate = { '94102': 288,
-  '94103': 491,
-  '94104': 22,
-  '94105': 65,
-  '94107': 158,
-  '94108': 71,
-  '94109': 180,
-  '94110': 278,
-  '94111': 73,
-  '94112': 158,
-  '94114': 98,
-  '94115': 112,
-  '94116': 49,
-  '94117': 151,
-  '94118': 69,
-  '94121': 53,
-  '94122': 102,
-  '94123': 70,
-  '94124': 212,
-  '94127': 36,
-  '94131': 48,
-  '94132': 52,
-  '94133': 139,
-  '94134': 71 
-  }
-
-    var maxMinArray = getMaxMin(aggregate);
-
-  // Generates a range of 9 values within the provided domain.
-  // Function used to generated CSS class values, 0-8.
-  // Values correspond to a range of blues in CSS file (see CSS file)
-  var quantize = d3.scale.quantize()
-      .domain(maxMinArray)
-      .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-        
-
-      // Render the heat map
-      svg.selectAll("path")
-      //   .data(gsfmap.features)
-      // .enter().append("path")
-      //   .attr("d", path)
-      //     .attr('data-id', function(d) {
-      //       return d.id;
-      //     }).attr('data-name', function(d) {
-      //       return d.properties.name;
-      //     })
-          .attr("class", function(d) {
-            console.log(d.id, quantize(aggregate[d.id]));
-            return quantize(aggregate[d.id]); // Quantize by total crimes per zipcode
-          });
-};
-
 
 // Renders standard map
 var render = function () {
@@ -189,5 +89,4 @@ var render = function () {
 
 
 render();
-renderHeatMap();
 renderPoints();
