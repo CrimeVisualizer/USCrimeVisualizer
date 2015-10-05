bf795f2d820d59f32cea0623967ae1b7e08d7ddf
Bowen Yang <carlyang007@gmail.com>
2015-10-05 16:42:16 -0700

    added searching category by date

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -4,12 +4,17 @@ var connection = require('../connection')
 var fs = require('fs');
 
 /* GET Crimes through api/events */
-router.get('/', function (req, res, next) {
+router.get('/date=:date', function (req, res, next) {
   // query DB for everything
   // will return you all the events in the database
+  var data = req.params.date.split('-');
+  var year = +data[0];
+  var month = +data[1];
+  var search = { Date: new RegExp('.*' + month + '\/.*\/' + year)};
+
   connection(function (db) {
 
-    db.collection('summarized_categorymonth').find().toArray(function(err, results) {
+    db.collection('summarized_categorymonth').find(search).toArray(function(err, results) {
       res.send(JSON.stringify(results));
     });
   });
